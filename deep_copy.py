b1c6747715e382129af4351879896df1add4b4f8
Syed Bilal Afzal <bilalafzal2004@hotmail.com>
2023-12-19 02:25:46 -0500

    deep_copy_2

--- deep_copy.py
+++ deep_copy.py
@@ -10,7 +10,7 @@
 
 #  Deep Copy
 
-def deep_copy(x):
+def deep_copy_1(x):
     y =[]
     for e in x:
         new_e = []
@@ -18,11 +18,32 @@ def deep_copy(x):
             new_e.append(sub_e)
         y.append(new_e)
     y[0][0] = 5
+    
+    
     print(id(x))
     print(id(y))
     print(id(a))
 
-a = [[1,2], [3,4], [5,6]]
-deep_copy(a)
-print(a)
-print(id(a))
+# a = [[1,2], [3,4], [5,6]]
+# deep_copy_1(a)
+# print(a)
+# print(id(a))
+
+
+def deep_copy_2(x):
+    y =[]
+    for e in x:
+        new_e = e[:]
+        y.append(new_e)
+    y[0][0] = 5
+    
+    
+    
+    print(id(x))
+    print(id(y))
+
+
+# a = [[1,2], [3,4], [5,6]]
+# deep_copy_2(a)
+# print(a)
+# print(id(a))
