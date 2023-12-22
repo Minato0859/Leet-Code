57ada96e01fdd24b9bb3b760e9e82d4d4acf59fa
Syed Bilal Afzal <bilalafzal2004@hotmail.com>
2023-12-22 08:42:44 -0500

    simple solution

--- LeetCode_75_studyplan/1- Array , String/1071. GCD of Strings.py
+++ LeetCode_75_studyplan/1- Array , String/1071. GCD of Strings.py
@@ -4,6 +4,17 @@
 # all strings that divides both str1 and str2 as divisible strings.
 # the longest string among all divisible strings as the GCD string.
 
+
+# simple sol:
+## from math import gcd
+# class Solution:
+#     def gcdOfStrings(self, str1: str, str2: str) -> str:
+#         if str1+str2 != str2+str1:
+#             return ""
+#         max_length = gcd(len(str1), len(str2))
+#         return str1[:max_length]
+
+
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
 
